9a1ea2513d5abc4d2452e34b4f94e0ad6a93bcd6
Syed Azlan <syedazlan5169@gmail.com>
2024-09-20 00:09:21 +0800

    update homepage styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -327,97 +327,7 @@
         <!-- Footer -->
         <footer class="mt-32 bg-gray-900 sm:mt-56" aria-labelledby="footer-heading">
         <h2 id="footer-heading" class="sr-only">Footer</h2>
-        <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-            <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-            <img class="h-7" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Company name">
-            <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-                <div class="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                    <h3 class="text-sm font-semibold leading-6 text-white">Solutions</h3>
-                    <ul role="list" class="mt-6 space-y-4">
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Marketing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Analytics</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Commerce</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Insights</a>
-                    </li>
-                    </ul>
-                </div>
-                <div class="mt-10 md:mt-0">
-                    <h3 class="text-sm font-semibold leading-6 text-white">Support</h3>
-                    <ul role="list" class="mt-6 space-y-4">
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Pricing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Documentation</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Guides</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">API Status</a>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-                <div class="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                    <h3 class="text-sm font-semibold leading-6 text-white">Company</h3>
-                    <ul role="list" class="mt-6 space-y-4">
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Blog</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Jobs</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Press</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Partners</a>
-                    </li>
-                    </ul>
-                </div>
-                <div class="mt-10 md:mt-0">
-                    <h3 class="text-sm font-semibold leading-6 text-white">Legal</h3>
-                    <ul role="list" class="mt-6 space-y-4">
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Claim</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Privacy</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Terms</a>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-            </div>
-            </div>
-            <div class="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between">
-            <div>
-                <h3 class="text-sm font-semibold leading-6 text-white">Subscribe to our newsletter</h3>
-                <p class="mt-2 text-sm leading-6 text-gray-300">The latest news, articles, and resources, sent to your inbox weekly.</p>
-            </div>
-            <form class="mt-6 sm:flex sm:max-w-md lg:mt-0">
-                <label for="email-address" class="sr-only">Email address</label>
-                <input type="email" name="email-address" id="email-address" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-56 sm:text-sm sm:leading-6" placeholder="Enter your email">
-                <div class="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-                <button type="submit" class="flex w-full items-center justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Subscribe</button>
-                </div>
-            </form>
-            </div>
+        <div class="mx-auto max-w-7xl px-6 pb-8 pt-8 sm:pt-12 lg:px-4 lg:pt-16">
             <div class="mt-8 border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
             <div class="flex space-x-6 md:order-2">
                 <a href="#" class="text-gray-500 hover:text-gray-400">
@@ -451,7 +361,11 @@
                 </svg>
                 </a>
             </div>
-            <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+            <div>
+                <p class="mt-1 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2024 onlinekad.com</p>
+                <p class="mt-1 text-xs leading-5 text-gray-400 md:order-2 md:mt-0">Dimiliki oleh: SY Digital Solutions</p>
+                <p class="mt-1 text-xs leading-5 text-gray-400 md:order-3 md:mt-0">BRN: 003369320-V</p>
+            </div>
             </div>
         </div>
         </footer>
