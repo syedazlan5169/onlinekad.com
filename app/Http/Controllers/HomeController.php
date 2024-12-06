592076c669f8bab94d0dcddac90b2cf814fa26de
Syed Azlan <syedazlan5169@gmail.com>
2024-12-06 18:33:43 +0800

    update home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Design;
+use App\Models\Package;
 
 class HomeController extends Controller
 {
@@ -11,7 +12,10 @@ class HomeController extends Controller
         // Fetch products from the database
         $products = Design::orderBy('total_created', 'desc')->limit(8)->get();
 
+        // Fetch products from the database
+        $packages = Package::all();
+
         // Pass products to the view
-        return view('home', compact('products'));
+        return view('home', compact('products', 'packages'));
     }
 }
