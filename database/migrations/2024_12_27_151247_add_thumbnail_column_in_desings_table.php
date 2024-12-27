e50212648b546e19e10764d59fefd3d7c8744c41
Syed Azlan <syedazlan5169@gmail.com>
2024-12-27 23:20:11 +0800

    update base_template to show different thumbnail for each design

---
+++ database/migrations/2024_12_27_151247_add_thumbnail_column_in_desings_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('designs', function (Blueprint $table) {
+            $table->string('thumbnail')->default('/images/kad-thumbnail.webp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('designs', function (Blueprint $table) {
+            $table->dropColumn('thumbnail');
+        });
+    }
+};
