bdbb0c027092fb93c3151fdcfc591d37a4976f89
Syed Azlan <syedazlan5169@gmail.com>
2024-12-29 14:19:10 +0800

    fix newkad mailing

--- resources/views/emails/new-kad.blade.php
+++ resources/views/emails/new-kad.blade.php
@@ -5,15 +5,15 @@
 </head>
 <!-- PHP variable -->
 @php
-    if($kadData == 3)
+    if($kadData['package_id'] == 3)
     {
         $pakej = 'Deluxe';
     }
-    if($kadData == 2)
+    if($kadData['package_id'] == 2)
     {
         $pakej = 'Premium';
     }
-    if($kadData == 1)
+    if($kadData['package_id'] == 1)
     {
         $pakej = 'Basic';
     }
@@ -29,7 +29,8 @@
             <td style="padding: 20px; text-align: left;">
                 <h1 style="color: #333;">Congratulation!</h1>
                 <p style="color: #555;">A Kad is successfully created</p>
-                <p>Tarikh Majlis: {{ $kadData->tarikh_majlis }}</p>
+                <p>Tarikh Majlis: {{ $kadData['tarikh_majlis'] }}</p>
+                <p>Design Id: {{ $kadData['design_id'] }}</p>
                 <p>Pakej: {{ $pakej }}</p>
                 <a href="staging.onlinekad.com/invitation/{{ $kadData->slug }}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #ffffff; text-decoration: none; border-radius: 4px;">
                     Check it out!! 
