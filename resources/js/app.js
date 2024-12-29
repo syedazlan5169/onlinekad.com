8abd8e57cc6624d79d4c5d8449d8ce5ee917f554
Syed Azlan <syedazlan5169@gmail.com>
2024-12-29 13:09:15 +0800

    added testimoni carousels

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,3 +17,35 @@ $(document).ready(function () {
         slidesToScroll: 1,
     });
 });
+
+$(document).ready(function () {
+    $('.testimoni-carousel').slick({
+        dots: true, // Show navigation dots
+        infinite: true, // Infinite looping
+        speed: 500, // Transition speed
+        slidesToShow: 3, // Slides visible at once
+        slidesToScroll: 1, // Number of slides to scroll
+        centerMode: true, // Center the active slide
+        centerPadding: '60px', // Add padding around slides
+        autoplay: true, // Enable autoplay
+        autoplaySpeed: 3000, // Autoplay delay
+        arrows: true, // Show next/prev arrows
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
+
