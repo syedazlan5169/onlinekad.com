a33ba4dd753b844b4df6f79e835f5625659e8535
logicgame1001 <logicgame1001@gmail.com>
2025-01-02 00:48:24 +0800

    test google login

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -74,7 +74,7 @@ new #[Layout('layouts.guest')] class extends Component
         </div>
     </form>
 
-    <!--<div>
+    <div>
         <div class="relative mt-4">
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
             <div class="w-full border-t border-gray-200"></div>
@@ -95,7 +95,7 @@ new #[Layout('layouts.guest')] class extends Component
             <span class="text-sm font-semibold leading-6">Continue With Google</span>
             </a>
         </div>
-    </div>-->
+    </div>
 
     <p class="mt-10 text-center text-sm text-gray-500">
       Don't have account?
