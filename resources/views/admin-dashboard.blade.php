800e2d83bae27d09885f8bd3fcd5a53377a39a60
Syed Azlan <syedazlan5169@gmail.com>
2025-03-18 21:15:05 +0800

    add edit and search function in admin dashboard kad table

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -93,7 +93,29 @@
             <!-- Kads Section -->
             <div x-data="{ deleteModal: false, selectedKadId: ''}" class="mt-20 max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="px-4 sm:px-6 lg:px-8">
-                    <h2 class="text-xl">Total Kads: {{ $totalKads }}</h2>
+                    <div class="sm:flex sm:items-center">
+                        <h2 class="text-xl">Total Kads: {{ $totalKads }}</h2>
+                        <!-- Add search form -->
+                        <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                            <form method="GET" action="" class="flex items-center space-x-4">
+                                <div class="relative rounded-md shadow-sm">
+                                    <input type="text" 
+                                           name="search" 
+                                           value="{{ request('search') }}"
+                                           class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                           placeholder="Search kads...">
+                                    <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+                                        <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    Search
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                     <!-- Users Section -->
                     <div class="mt-2 flow-root">
                         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -111,7 +133,7 @@
                                                 <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Payment</th>
                                                 <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Design</th>
                                                 <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Link</th>
+                                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Action</th>
                                             </tr>
                                         </thead>
                                         <tbody class="divide-y divide-gray-200 bg-white">
@@ -163,7 +185,7 @@
                                                 @else
                                                     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium border border-l text-gray-900 sm:pl-6">{{ $kad->nama_panggilan_lelaki }} & {{ $kad->nama_panggilan_perempuan }}</td>
                                                 @endif
-                                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium border border-l text-blue-500 sm:pl-6"><a href="/invitation/{{ $kad->slug }}">View</a></td>
+                                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium border border-l sm:pl-6"><a class="text-blue-500" href="/invitation/{{ $kad->slug }}">View</a><a class="ml-2 text-green-500" href="/kad-edit/{{ $kad->id }}">Edit</a></td>
                                             </tr>
                                             @endforeach
                                         </tbody>
